80c50538229ac3871d49bceee61487d447c83275
muhajirmuhajir <hajir.nf@gmail.com>
2020-01-14 22:01:21 +0700

    add Customer controller

--- src/controllers/CustomerController.js
+++ src/controllers/CustomerController.js
@@ -4,5 +4,11 @@ const mongoose = require('mongoose');
 const Customer = mongoose.model('Customer');
 
 exports.register = (req, res) => {
+    var user = new Customer(req.body);
+    user.save((err, model) => {
+        if (err)
+            res.send(err);
 
-}
+        res.json(model);
+    });
+};
