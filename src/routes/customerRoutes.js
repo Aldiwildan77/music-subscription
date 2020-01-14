a26668d4b5e62f9dc212dfb58144713b719c7e6c
muhajirmuhajir <hajir.nf@gmail.com>
2020-01-14 21:47:35 +0700

    add Customer routes

--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -2,13 +2,17 @@
 exports.route = (app) => {
     var customer = require('../controllers/CustomerController');
 
+
     app.route('/customer')
         .post(customer.register);
+
 
     app.route('/customer/:userId')
         .get(customer.get_user_info)
         .put(customer.top_up);
 
+
     app.route('/customer/debit/:userId')
         .put(customer.debit);
+
 };
