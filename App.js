9fdb3387fcff6455f00e5c79cf421d2a0012c12f
muhajirmuhajir <hajir.nf@gmail.com>
2020-01-16 00:26:24 +0700

    deploy local database to online

--- App.js
+++ App.js
@@ -10,11 +10,16 @@ const subscriptionRoute = require('./src/routes/subscriptionRoutes');
 const transactionRoute = require('./src/routes/transactionRoutes');
 
 
+var online = 'mongodb://admin:Dp9KNJLuCNGTcoKV@cluster0-shard-00-00-uavp4.mongodb.net:27017,cluster0-shard-00-01-uavp4.mongodb.net:27017,cluster0-shard-00-02-uavp4.mongodb.net:27017/local_library?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb', {
+mongoose.connect(online, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -25,4 +30,4 @@ customerRoute.route(app);
 subscriptionRoute.route(app);
 transactionRoute.route(app);
 
-app.listen(3000);
+app.listen(8000);
